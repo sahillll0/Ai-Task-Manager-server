6518fac24bd8ae75c8fe468c09cf3aa60d45c2ba
sahillll0 <sahiltippe143@gmail.com>
2025-12-20 15:31:57 +0530

    file update

--- src/index.js
+++ src/index.js
@@ -2,14 +2,16 @@ import "dotenv/config";
 import app from "./app.js";
 import connectDB from "./db/db.js";
 
-
 connectDB()
-.then(()=>{
-    app.listen(process.env.PORT,()=>{
+  .then(() => {
+    if (process.env.NODE_ENV !== "production") {
+      app.listen(process.env.PORT, () => {
         console.log(`Server is running on port ${process.env.PORT}`);
-    })
-})
-.catch(()=>{
-    console.log("failed to connect server");
-    
-})
+      });
+    }
+  })
+  .catch((err) => {
+    console.log("failed to connect server", err);
+  });
+
+export default app;
